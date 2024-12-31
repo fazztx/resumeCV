fdcf7cd154f2279f56bfb7f9fdc3f79da6fa8610
fazztx <juan.software@outlook.com>
2024-12-30 23:48:11 -0400

    Deployment fixes.

--- resume-project/vite.config.js
+++ resume-project/vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/resumeCV',
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
+});
